1f48e75879cb81122d7e28b6096ac739a519d562
김민정 <pacedov3@gmail.com>
2023-01-03 01:41:26 +0900

    [2주차] 완전탐색 & 백트래킹 응용

--- Kming9/[2주차] 완전탐색 + 백트래킹 응용/BOJ_16571_알파_틱택토.java
+++ Kming9/[2주차] 완전탐색 + 백트래킹 응용/BOJ_16571_알파_틱택토.java
@@ -6,15 +6,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_16571_알파_틱택토 {
-    static int player, map[][], count[], N = 3;
-    static char ans;
+    static int map[][], count[], N = 3;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         map = new int[N][N];
         count = new int[N];
-        ans = 'D';
 
         for (int y = 0; y < N; y++) {
             st = new StringTokenizer(br.readLine());
@@ -24,45 +23,43 @@ public class BOJ_16571_알파_틱택토 {
             }
         }
 
-        player = count[1] <= count[2] ? 1 : 2;
-        dfs(player, count[0]);
+        int player = count[1] <= count[2] ? 1 : 2;
+        int ans = dfs(player, count[0]);
 
-        System.out.println(ans);
+        System.out.println(ans == 1 ? 'W' : ans == 0 ? 'D' : 'L');
     }
 
-    public static void dfs(int num, int cnt) {
+    public static int dfs(int player, int cnt) {
         int res = play();
-        if(res == player) {
-            ans = 'W';
-            return;
-        } else if(res > 0){
-            ans = 'L';
-            return;
-        }
-        
-        if(ans != 'D' || cnt == 0)  return;
-        
-        for(int y=0; y<N; y++) {
-            for(int x=0; x<N; x++) {
-                if(map[y][x] != 0) continue;
 
-                int nextPlayer = num == 1 ? 2 : 1;
-                map[y][x] = num;
-                dfs(nextPlayer, cnt-1);
+        if (res == player) return 1;
+        else if (res > 0) return -1;
+        if (cnt == 0) return 0;
+
+        int max = -1;
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < N; x++) {
+                if (map[y][x] != 0) continue;
+
+                int nextPlayer = player == 1 ? 2 : 1;
+                map[y][x] = player;
+                max = Math.max(max, -dfs(nextPlayer, cnt - 1));
                 map[y][x] = 0;
             }
         }
+
+        return max;
     }
-    
+
     public static int play() {
-        for(int i=0; i<N; i++)
-           if(map[i][0] != 0 && map[i][0] == map[i][1] && map[i][1] == map[i][2]) return map[i][0];
+        for (int i = 0; i < N; i++)
+            if (map[i][0] != 0 && map[i][0] == map[i][1] && map[i][1] == map[i][2]) return map[i][0];
 
-        for(int i=0; i<N; i++)
-            if(map[0][i] != 0 && map[0][i] == map[1][i] && map[1][i] == map[2][i]) return map[0][i];
+        for (int i = 0; i < N; i++)
+            if (map[0][i] != 0 && map[0][i] == map[1][i] && map[1][i] == map[2][i]) return map[0][i];
 
-        if(map[0][0] != 0 && map[0][0] == map[1][1] && map[1][1] == map[2][2]) return map[1][1];
-        if(map[0][0] != 0 && map[0][2] == map[1][1] && map[1][1] == map[2][0]) return map[1][1];
+        if (map[0][0] != 0 && map[0][0] == map[1][1] && map[1][1] == map[2][2]) return map[1][1];
+        if (map[0][0] != 0 && map[0][2] == map[1][1] && map[1][1] == map[2][0]) return map[1][1];
 
         return -1;
     }
