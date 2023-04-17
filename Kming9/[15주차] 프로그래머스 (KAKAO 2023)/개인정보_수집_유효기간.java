1fb4e7199ad03238ecee768ce1f725e0c9473639
김민정 <43158727+Kming9@users.noreply.github.com>
2023-04-17 22:12:44 +0900

    [15주차] 프로그래머스 (kakao 2023) (#43)

---
+++ Kming9/[15주차] 프로그래머스 (KAKAO 2023)/개인정보_수집_유효기간.java
@@ -0,0 +1,33 @@
+import java.util.;
+import java.time.;
+import java.time.format.DateTimeFormatter;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayListInteger ansList = new ArrayList();
+        
+        DateTimeFormatter fm = DateTimeFormatter.ofPattern(yyyy.MM.dd);
+        LocalDate curDate = LocalDate.parse(today, fm);
+        LocalDate std = curDate.getDayOfMonth() == 28 
+             LocalDate.of(curDate.getYear(), curDate.getMonth().plus(1), 1)  curDate.plusDays(1);
+
+        HashMapString, Integer hm = new HashMap();
+        for(String t  terms) {
+            String[] info = t.split( );
+            hm.put(info[0], Integer.parseInt(info[1]));
+        }
+
+        int i = 1;
+        for (String p  privacies) {
+            String[] info = p.split( );
+            LocalDate date = LocalDate.parse(info[0], fm).plusMonths(hm.get(info[1]));
+            
+            if(date.isBefore(std))
+                ansList.add(i);
+            
+            i++;
+        }
+
+        return ansList.stream().mapToInt(n - n).toArray();
+    }
+}
